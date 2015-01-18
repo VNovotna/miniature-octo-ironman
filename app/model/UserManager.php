0ec936a81a5d1f31de20d42424336dc7c840b76e
Viky Novotných <viky@G580-V>
2015-01-18 12:08:02 +0100

    very early preparation work for layout switcher

--- app/model/UserManager.php
+++ app/model/UserManager.php
@@ -3,7 +3,6 @@
 namespace App\Model;
 
 use Nette,
-    Nette\Utils\Strings,
     Nette\Security\Passwords;
 
 /**
--- app/router/RouterFactory.php
+++ app/router/RouterFactory.php
@@ -2,26 +2,21 @@
 
 namespace App;
 
-use Nette,
-	Nette\Application\Routers\RouteList,
-	Nette\Application\Routers\Route,
-	Nette\Application\Routers\SimpleRouter;
-
+use Nette\Application\Routers\RouteList,
+    Nette\Application\Routers\Route;
 
 /**
  * Router factory.
  */
-class RouterFactory
-{
+class RouterFactory {
 
-	/**
-	 * @return \Nette\Application\IRouter
-	 */
-	public static function createRouter()
-	{
-		$router = new RouteList();
-		$router[] = new Route('<presenter>/<action>[/<id>]', 'Homepage:default');
-		return $router;
-	}
+    /**
+     * @return \Nette\Application\IRouter
+     */
+    public static function createRouter() {
+        $router = new RouteList();
+        $router[] = new Route('<presenter>/<action>[/<id>]', 'Homepage:default');
+        return $router;
+    }
 
 }
