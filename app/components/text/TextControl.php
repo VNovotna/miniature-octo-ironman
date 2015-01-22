28adf6a7ac317e75f7c4f2512ccaf08f8ad791f1
Viky Novotných <viky@G580-V>
2015-01-22 12:33:52 +0100

    TextControl read text from db

--- app/components/text/TextControl.php
+++ app/components/text/TextControl.php
@@ -9,7 +9,15 @@ namespace App\Components\Text;
  */
 class TextControl extends \App\Components\GenericComponent {
 
+    public function loadText() {
+        $context = new \Nette\Database\Context(new \Nette\Database\Connection('sqlite:../app/components/text/db'));
+        $table = $context->table('data');
+        $row = $table->where(array('component_id' => $this->id))->fetch();
+        return $row->text;
+    }
+
     public function render() {
+        $this->template->text = $this->loadText();
         if ($this->edit == TRUE) {
             $this->template->setFile(__DIR__ . '/text-edit.latte');
             $this->template->render();
