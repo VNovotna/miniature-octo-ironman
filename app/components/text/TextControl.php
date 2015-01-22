34f42efc2c619524dcd6e33a0cdcd4f024cbd829
Viky Novotných <viky@G580-V>
2015-01-22 17:12:50 +0100

    fooling around with forms

--- app/components/text/TextControl.php
+++ app/components/text/TextControl.php
@@ -9,6 +9,22 @@ namespace App\Components\Text;
  */
 class TextControl extends \App\Components\GenericComponent {
 
+    /**
+     * @var string
+     */
+    protected $text = "";
+
+    /**
+     * @param int $id
+     */
+    public function setId($id) {
+        $this->id = $id;
+        $this->text = $this->loadText();
+    }
+
+    /**
+     * @return string
+     */
     public function loadText() {
         $context = new \Nette\Database\Context(new \Nette\Database\Connection('sqlite:../app/components/text/db'));
         $table = $context->table('data');
@@ -16,8 +32,30 @@ class TextControl extends \App\Components\GenericComponent {
         return $row->text;
     }
 
+    /**
+     * @return \Nette\Application\UI\Form
+     */
+    protected function createComponentEditForm() {
+        $form = new \Nette\Application\UI\Form();
+        $form->addTextArea('text', '')
+                ->setValue($this->text)
+                ->setAttribute('style', 'width:100%;min-height:120px');
+        $form->addSubmit('send');
+        $form->onSuccess[] = $this->editFormSubmitted;
+        return $form;
+    }
+
+    /**
+     * @param \Nette\Application\UI\Form $form
+     */
+    public function editFormSubmitted(\Nette\Application\UI\Form $form) {
+        /* dump($form); */
+        $this->getPresenter()->flashMessage('changes discarted :P');
+        $this->redirect('this');
+    }
+
     public function render() {
-        $this->template->text = $this->loadText();
+        $this->template->text = $this->text;
         if ($this->edit == TRUE) {
             $this->template->setFile(__DIR__ . '/text-edit.latte');
             $this->template->render();
